887e4f7f0bcdaa59e76ed6be6c8598af26dd2282
Deena Smith <deenasmith1630@gmail.com>
2022-03-18 10:02:29 -0500

    removed config file, rearranged variables--APOD is now visible to all users

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,13 @@
 // Blast Off click randomly generates image from API 
 var imageBtn = document.getElementById('exploreBtn');
 var spaceImage = document.getElementById('images');
-var imageUrl = 'https://api.nasa.gov/planetary/apod?api_key='
-var imageKey = config.nasa_api_key
+var imageUrl = 'https://api.nasa.gov/planetary/apod?api_key=Jmooles2hiSkfkS4ibQMjXPovBcmyRlbq31RfhiQ'
 
 
 imageBtn.addEventListener('click', function () {
     const fetchNASAData = async () => {
         try {
-            const response = await fetch(imageUrl + imageKey)
+            const response = await fetch(imageUrl)
             const data = await response.json()
         
             nasaImage(data) 
